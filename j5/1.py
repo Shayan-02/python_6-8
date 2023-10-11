d6cd537bc22b1f37892cafa4fc0eb7f00a4244b2
Shayan-02 <upm200@gmail.com>
2023-10-11 20:06:45 +0330

    j6

--- j5/1.py
+++ j5/1.py
@@ -1,6 +1,9 @@
 a = 20
+b = "hello"
 for i in range (a, 26):
     print(i, end="\n\n")
+for j in b:
+    print(j, end=" ")
 # [x, y] ---> x, x+1, x+2, ...., y
 # [x, y) ---> x, x+1, x+2, ...., y-1 ---> for i in range():
 # print()
---
+++ j5/6.py
@@ -0,0 +1,11 @@
+a = "mohammad"
+lst = ["ali"]
+# [m, o, h, a, m, m, a, d]
+for i in a:
+    lst.append(i)
+
+for _ in lst:
+    print(_, end=" ")
+
+a = 20
+b = 20
